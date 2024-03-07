5d36e3a6e1315c97620fae3641e954e4e14d8a39
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2024-03-06 23:23:12 -0300

    Add index support

--- src/SimpleSitemap.php
+++ src/SimpleSitemap.php
@@ -2,7 +2,32 @@
 
 namespace SebaCarrasco93\SimpleSitemap;
 
+use Illuminate\Http\Response;
+use SebaCarrasco93\SimpleSitemap\Generator\SitemapIndex;
+use SebaCarrasco93\SimpleSitemap\Generator\UrlSitemapIndex;
+
 class SimpleSitemap
 {
-    
+    public function __construct(public SitemapIndex $sitemap_index)
+    {
+    }
+
+    public function checkRoutes(array $routes = [])
+    {
+        if (! count($routes)) {
+            throw new Exceptions\EmptyRoutes();
+        }
+    }
+
+    public function index(array $routes = []): Response
+    {
+        $this->checkRoutes($routes);
+        
+        foreach ($routes as $route) {
+            $this->sitemap_index->add(UrlSitemapIndex::create($route));
+        }
+
+        return response($this->sitemap_index->build())
+            ->header('Content-Type', 'text/xml');
+    }
 }
